23930af1d8bc540aa0b90ee0f657cfa2b5aaf400
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2023-07-17 14:07:41 +0100

    commit change

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const seo = {
 const greeting = {
   title: "Patrick Vieira",
   logo_name: "Patrick Vieira",
-  nickname: "layman_brother",
+  nickname: "Patrickvieira",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
