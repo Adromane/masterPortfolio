f37123802659d8b0b1aaf1da560231327bf06e82
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2023-07-18 04:30:33 +0100

    commit change

--- src/portfolio.js
+++ src/portfolio.js
@@ -726,7 +726,7 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "animated_ashutosh.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "Je suis disponible sur presque tous les réseaux sociaux. Vous pouvez m'envoyer un message, je vous répondrai dans les 24 heures.",
   },
   blogSection: {
     title: "Blogs",
