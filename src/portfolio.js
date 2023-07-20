79d40ac84cadae49827777a8c77bcb6d4e49cde6
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2023-07-20 03:37:20 +0100

    commit change

--- src/portfolio.js
+++ src/portfolio.js
@@ -418,7 +418,7 @@ const experience = {
   title: "Experience",
   subtitle: "Work, Internship and Volunteership",
   description:
-    "I have worked with many evolving startups as ML and DL Developer, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
+    "J'ai toujours fait de mon mieux pour me mettre à jour et rester au fait des nouvelles technologies, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
   header_image_path: "experience.svg",
   sections: [
     {
