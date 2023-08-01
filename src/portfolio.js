cdd34f6333e76ac91570d9da6ee416574171e0ec
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2023-08-01 16:20:56 +0100

    commit push

--- src/portfolio.js
+++ src/portfolio.js
@@ -372,7 +372,7 @@ const experience = {
   title: "Experience",
   subtitle: "Travail, stage et volontariat",
   description:
-    "C'est avec une grande satisfaction que je vous présente mon projet, fruit de ma passion inébranlable pour l'innovation technologique et de mon engagement à suivre de près les tendances émergentes du monde professionnel. Mon parcours de programmation a commencé avec HTML, CSS et JS à l'école, mais mon désir insatiable de connaissances m'a amené à explorer les langages Java et Python.",
+    "C'est avec une grande satisfaction que je vous présente mon projet, fruit de ma passion inébranlable pour l'innovation technologique et de mon engagement à suivre de près les tendances émergentes du monde professionnel. Mon parcours de programmation a commencé avec HTML, CSS et JS à l'école, mais mon désir insatiable de connaissances m'a amené à explorer les langages Java et Python...",
   header_image_path: "experience.svg",
   sections: [
     {
