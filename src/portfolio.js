cfc299ecea12bfaaa9e0383cc1140cfad987e663
Patrick Vieira <107551156+Adromane@users.noreply.github.com>
2023-09-23 22:24:55 +0100

    commit change

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "Je suis un(e) étudiant(e) passionné(e) à la recherche d'opportunités de stages pour acquérir une expérience pratique dans mon domaine d'intérêt. Bien que je n'aie pas encore d'expérience professionnelle formelle.",
   resumeLink:
-    "https://drive.google.com/file/d/1FL_wydnPF2SWCdw2goYAgZ1KKBx8TpcD/view?usp=sharing",
+    "https://drive.google.com/file/d/1FL_wydnPF2SWCdw2goYAgZ1KKBx8TpcD/view?usp=sharingfile:///C:/Users/vieir/Downloads/CV-Patrik%20A.Vieira%20Gomes.pdf",
   portfolio_repository: "https://github.com/Adromane",
   githubProfile: "https://github.com/Adromane",
 };
